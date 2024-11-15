869ee4ec760284df8883b147451ccf095bc660dd
Austin Heil <austinmh12@gmail.com>
2024-11-14 20:27:36 -0500

    Documentation and changed ability_type to type

--- src/card/models/ability.rs
+++ src/card/models/ability.rs
@@ -3,8 +3,10 @@ use serde::{Deserialize, Serialize};
 /// Represents an ability on a card.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Ability {
+	/// The name of the ability.
 	pub name: String,
+	/// The text value of the ability.
 	pub text: String,
-	#[serde(rename = "type")]
-	pub ability_type: String,
+	/// The type of the ability, such as Ability or Pokémon-Power.
+	pub r#type: String,
 }
